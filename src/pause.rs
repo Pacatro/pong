018b43566f8a90002415c3d7a7c37724326ef4f4
Pacatro <paco.algar.15@gmail.com>
2024-03-02 15:20:57 +0100

    adding pause button

--- src/pause.rs
+++ src/pause.rs
@@ -2,16 +2,22 @@ use bevy::prelude::*;
 
 use crate::GameState;
 
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+
 pub struct PausePlugin;
 
 impl Plugin for PausePlugin {
     fn build(&self, app: &mut App) {
         app
             .add_systems(Update, set_pause)
-            .add_systems(OnEnter(GameState::Pause), pause)
-            .add_systems(Update, set_in_game.run_if(in_state(GameState::Pause)));
+            .add_systems(OnEnter(GameState::Pause), spawn_pause_menu)
+            .add_systems(Update, set_in_game.run_if(in_state(GameState::Pause)))
+            .add_systems(OnExit(GameState::Pause), delete_pause_menu);
     }
 }
+
+#[derive(Debug, Component)]
+pub struct PauseButton;
 
 fn set_pause(
     mut game_state: ResMut<NextState<GameState>>,
@@ -33,8 +39,60 @@ fn set_in_game(
     }
 }
 
-fn pause(
+fn spawn_pause_menu(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
     mut time: ResMut<Time<Virtual>>
 ) {
     time.pause();
+
+    commands.spawn((
+        NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                ..default()
+            },
+            ..default()
+        },
+        PauseButton
+    ))
+        .with_children(|parent| {
+            parent
+                .spawn(ButtonBundle {
+                    style: Style {
+                        width: Val::Px(400.0),
+                        height: Val::Px(100.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    border_color: BorderColor(Color::WHITE),
+                    background_color: NORMAL_BUTTON.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "PRESS SPACE TO CONTINUE",
+                        TextStyle {
+                            font: asset_server.load("fonts/I-pixel-u.ttf"),
+                            font_size: 40.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    ));
+                });
+        });
+}
+
+fn delete_pause_menu(
+    mut commands: Commands,
+    query: Query<Entity, With<Button>>
+) {
+    let button = query.single();
+    commands.entity(button).despawn_recursive()
 }
